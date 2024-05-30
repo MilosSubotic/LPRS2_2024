5efa7dabd2a10ccb05a4bd0997dae81db09d2229
antovic <vuk.antovic48@gmail.com>
2024-05-30 09:53:48 +0200

    Movement on both steppers, added defines

--- Automotive/Diesel_Generator/steppers/steppers.ino
+++ Automotive/Diesel_Generator/steppers/steppers.ino
@@ -1,31 +1,44 @@
-#include <AFMotor.h>  
-//https://www.instructables.com/Arduino-L293D-Motor-Driver-Shield-Tutorial/
-AF_Stepper motor(48, 2);
+#include <AFMotor.h>
+
+#define MOTOR_STEP 48
+#define MOTOR_1 1
+#define MOTOR_2 2
+#define MOTOR_SPEED 100
+
+
+AF_Stepper motorBlack(MOTOR_STEP, MOTOR_1); // Define stepper motor object for motorBlack
+AF_Stepper motorWhite(MOTOR_STEP, MOTOR_2); // Define stepper motor object for motorWhite
 
 void setup() {
-motor.setSpeed(300);
+  motorBlack.setSpeed(MOTOR_SPEED);
+  motorBlack.onestep(FORWARD, SINGLE);
+  motorBlack.release();
 
-motor.onestep(FORWARD, SINGLE);
+  motorWhite.setSpeed(MOTOR_SPEED);
+  motorWhite.onestep(FORWARD, SINGLE);
+  motorWhite.release();
 
-motor.release();
-
-delay(1000);
-
+  delay(1000);
 }
 
-
 void loop() {
-motor.step(100, FORWARD, SINGLE);
+  // Steps for motorBlack
+  motorBlack.step(100, FORWARD, SINGLE);
+  motorBlack.step(100, BACKWARD, SINGLE);
+  motorBlack.step(100, FORWARD, DOUBLE);
+  motorBlack.step(100, BACKWARD, DOUBLE);
+  motorBlack.step(100, FORWARD, INTERLEAVE);
+  motorBlack.step(100, BACKWARD, INTERLEAVE);
+  motorBlack.step(100, FORWARD, MICROSTEP);
+  motorBlack.step(100, BACKWARD, MICROSTEP);
 
-motor.step(100, BACKWARD, SINGLE);
-
-motor.step(100, FORWARD, DOUBLE);
-motor.step(100, BACKWARD, DOUBLE);
-
-motor.step(100, FORWARD, INTERLEAVE);
-motor.step(100, BACKWARD, INTERLEAVE);
-
-motor.step(100, FORWARD, MICROSTEP);
-motor.step(100, BACKWARD, MICROSTEP);
-
+  // Steps for motorWhite
+  motorWhite.step(100, FORWARD, SINGLE);
+  motorWhite.step(100, BACKWARD, SINGLE);
+  motorWhite.step(100, FORWARD, DOUBLE);
+  motorWhite.step(100, BACKWARD, DOUBLE);
+  motorWhite.step(100, FORWARD, INTERLEAVE);
+  motorWhite.step(100, BACKWARD, INTERLEAVE);
+  motorWhite.step(100, FORWARD, MICROSTEP);
+  motorWhite.step(100, BACKWARD, MICROSTEP);
 }
